0d78f8130c09a4a7a516e5922b16090a0db52ccb
tsatolol <suika.1106@gmail.com>
2025-09-15 01:48:49 +0900

    .

---
+++ functions/test_d1.js
@@ -0,0 +1,7 @@
+export async function onRequest(context) {
+  // Create a prepared statement with our query
+  const ps = context.env.prod_d1_tutorial.prepare("SELECT * from users");
+  const data = await ps.first();
+
+  return Response.json(data);
+}
